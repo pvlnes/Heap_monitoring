7c5b565cba7eb8c4e7d3b89ceb7a41491cd28259
Pavel Nesterov <Pavel.Nesterov@kaspersky.com>
2020-03-13 20:35:38 +0300

    add 32-bit arch and few fixes

--- mem_alloc.py
+++ mem_alloc.py
@@ -1,9 +1,10 @@
 import pykd
-home = ".//"
+import os
+from os.path import expanduser
+home = expanduser("~")
 return_reg = "rax"
 stack_pointer = "rsp"
 log = None
-
 
 #RtlAllocateHeap - routine allocates a block of memory from heap(
 # IN PVOID                HeapHandle,
@@ -20,10 +21,17 @@ class handle_allocate_heap(pykd.eventHandler):
 
     def enter_call_back(self):
         self.out = "RtlAllocateHeap("
-        self.out += hex(pykd.reg("rcx")) + " , "
-        self.out += hex(pykd.reg("rdx")) + " , "
-        self.out += hex(pykd.reg("r8")) + ") = "
-        if self.bp_end == None:
+        if arch_bits == 32:
+            esp = pykd.reg(stack_pointer)
+            self.out += hex(pykd.ptrPtr(esp + 4)) + " , "
+            self.out += hex(pykd.ptrMWord(esp + 0x8)) + " , "
+            self.out += hex(pykd.ptrMWord(esp + 0xC)) + ") = "
+        else:
+            self.out += hex(pykd.reg("rcx")) + " , "
+            self.out += hex(pykd.reg("rdx")) + " , "
+            self.out += hex(pykd.reg("r8")) + ") = "
+
+        if self.bp_end is None:
             disas = pykd.dbgCommand("uf ntdll!RtlAllocateHeap").split('\n')
             for i in disas:
                 if 'ret' in i:
@@ -35,12 +43,11 @@ class handle_allocate_heap(pykd.eventHandler):
     def return_call_back(self):
         log.write(self.out + hex(pykd.reg(return_reg)) + "\n")
         return False
-
-
 #RtlFreeHeap - routine frees a memory block that was allocated from a heap by RtlAllocateHeap(
 #IN PVOID                HeapHandle,
 #IN ULONG                Flags OPTIONAL,
 #IN PVOID                MemoryPointer );
+
 class handle_free_heap(pykd.eventHandler):
         bp_end = None
         def __init__(self):
@@ -51,7 +58,13 @@ class handle_free_heap(pykd.eventHandler):
                 self.bp_end = None
 
         def enter_call_back(self):
-                self.out = "RtlFreeHeap("
+            self.out = 'RtlFreeHeap('
+            if arch_bits == 32:
+                esp = pykd.reg(stack_pointer)
+                self.out += hex(pykd.ptrPtr(esp + 4)) + " , "
+                self.out += hex(pykd.ptrMWord(esp + 0x8)) + " , "
+                self.out += hex(pykd.ptrPtr(esp + 0xC)) + ") = "
+            else:
                 self.out += hex(pykd.reg("rcx")) + " , "
                 self.out += hex(pykd.reg("rdx")) + " , "
                 self.out += hex(pykd.reg("r8")) + ") = "
@@ -83,7 +96,14 @@ class handle_realloc_heap(pykd.eventHandler):
                 self.bp_end = None
 
         def enter_call_back(self):
-                self.out = "RtlReAllocateHeap("
+            self.out = "RtlReAllocateHeap("
+            if arch_bits == 32:
+                esp = pykd.reg(stack_pointer)
+                self.out += hex(pykd.ptrPtr(esp + 4)) + " , "
+                self.out += hex(pykd.ptrMWord(esp + 0x8)) + " , "
+                self.out += hex(pykd.ptrPtr(esp + 0xC)) + " , "
+                self.out += hex(pykd.ptrMWord(esp + 0x10)) + ") = "
+            else:
                 self.out += hex(pykd.reg("rcx")) + " , "
                 self.out += hex(pykd.reg("rdx")) + " , "
                 self.out += hex(pykd.reg("r8")) + " , "
